0270278f4353123313fea9276f444b589377774e
hex7c0 <carniellifrancesco@gmail.com>
2014-12-05 16:11:07 +0100

    add first example

---
+++ examples/login.js
@@ -0,0 +1,25 @@
+'use strict';
+/**
+ * @file login example
+ * @module mongodb-backup
+ * @package mongodb-backup
+ * @subpackage examples
+ * @version 0.0.0
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+var backup = require('..'); // use require('mongodb-backup') instead
+
+/*
+ * use
+ */
+backup({
+  uri: 'uri',// mongodb://<dbuser>:<dbpassword>@<dbdomain>.mongolab.com:<dbport>/<dbdatabase>
+  root: __dirname,
+  collections: [ 'logins' ]
+});
