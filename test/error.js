e8581f6d38f89ea345dfabf383aca931bdbe7547
hex7c0 <carniellifrancesco@gmail.com>
2014-12-05 17:32:01 +0100

    add tests

---
+++ test/error.js
@@ -0,0 +1,76 @@
+'use strict';
+/**
+ * @file error test
+ * @module mongodb-backup
+ * @package mongodb-backup
+ * @subpackage test
+ * @version 0.0.1
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+try {
+  var monitode = require('..');
+  var assert = require('assert');
+} catch (MODULE_NOT_FOUND) {
+  console.error(MODULE_NOT_FOUND);
+  process.exit(1);
+}
+
+/*
+ * test module
+ */
+describe('error', function() {
+
+  it('should return missing uri', function(done) {
+
+    var mex = 'missing uri options';
+    try {
+      monitode();
+    } catch (e) {
+      assert.equal(e.message, mex);
+    }
+    try {
+      monitode({});
+    } catch (e) {
+      assert.equal(e.message, mex);
+    }
+    try {
+      monitode({
+        root: 'ciao'
+      });
+    } catch (e) {
+      assert.equal(e.message, mex);
+    }
+    done();
+  });
+  it('should return missing root', function(done) {
+
+    var mex = 'missing root options';
+    try {
+      monitode({
+        uri: 'ciao'
+      });
+    } catch (e) {
+      assert.equal(e.message, mex);
+    }
+    done();
+  });
+  it('should return wrong uri', function(done) {
+
+    var mex = 'URL must be in the format mongodb://user:pass@host:port/dbname';
+    try {
+      monitode({
+        uri: 'ciao',
+        root: 'ciao'
+      });
+    } catch (e) {
+      assert.equal(e.message, mex);
+    }
+    done();
+  });
+});
---
+++ test/simple.js
@@ -0,0 +1,167 @@
+'use strict';
+/**
+ * @file simple test
+ * @module mongodb-backup
+ * @package mongodb-backup
+ * @subpackage test
+ * @version 0.0.1
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+try {
+  var monitode = require('..');
+  var assert = require('assert');
+  var fs = require('fs');
+  var extname = require('path').extname;
+  var URI = process.env.URI;
+} catch (MODULE_NOT_FOUND) {
+  console.error(MODULE_NOT_FOUND);
+  process.exit(1);
+}
+
+/*
+ * test module
+ */
+describe('simple', function() {
+
+  var ROOT = __dirname + '/dump';
+  this.timeout(10000);
+
+  describe('collections', function() {
+
+    it('should build 1 directory (*.json)', function(done) {
+
+      monitode({
+        uri: URI,
+        root: ROOT,
+        collections: [ 'logins' ],
+        callback: function() {
+
+          fs.readdirSync(ROOT).forEach(function(first) { // database
+
+            var second = ROOT + '/' + first;
+            if (!fs.statSync(second).isDirectory()) {
+              return;
+            }
+            var third = fs.readdirSync(second);
+            assert.equal(third.length, 1);
+            third = third[0];
+            assert.equal(third, 'logins');
+            var last = second + '/' + third;
+            if (!fs.statSync(last).isDirectory()) {
+              return;
+            }
+            fs.readdirSync(last).forEach(function(item) { // documents
+
+              assert.equal(extname(item), '.json');
+              fs.unlinkSync(last + '/' + item);
+            });
+          });
+          done();
+        }
+      });
+    });
+    it('should build 1 directory (*.bson)', function(done) {
+
+      monitode({
+        uri: URI,
+        root: ROOT,
+        collections: [ 'logins' ],
+        parser: 'bson',
+        callback: function() {
+
+          fs.readdirSync(ROOT).forEach(function(first) { // database
+
+            var second = ROOT + '/' + first;
+            if (!fs.statSync(second).isDirectory()) {
+              return;
+            }
+            var third = fs.readdirSync(second);
+            assert.equal(third.length, 1);
+            third = third[0];
+            assert.equal(third, 'logins');
+            var last = second + '/' + third;
+            if (!fs.statSync(last).isDirectory()) {
+              return;
+            }
+            fs.readdirSync(last).forEach(function(item) { // documents
+
+              assert.equal(extname(item), '.bson');
+              fs.unlinkSync(last + '/' + item);
+            });
+          });
+          done();
+        }
+      });
+    });
+  });
+  describe('parser', function() {
+
+    it('should build any directories (*.json)', function(done) {
+
+      monitode({
+        uri: URI,
+        root: ROOT,
+        callback: function() {
+
+          fs.readdirSync(ROOT).forEach(function(first) { // database
+
+            var second = ROOT + '/' + first;
+            if (!fs.statSync(second).isDirectory()) {
+              return;
+            }
+            fs.readdirSync(second).forEach(function(third) { // collections
+
+              var last = second + '/' + third;
+              if (!fs.statSync(last).isDirectory()) {
+                return;
+              }
+              fs.readdirSync(last).forEach(function(item) { // documents
+
+                assert.equal(extname(item), '.json');
+                fs.unlinkSync(last + '/' + item);
+              });
+            });
+          });
+          done();
+        }
+      });
+    });
+    it('should build any directories (*.bson)', function(done) {
+
+      monitode({
+        uri: URI,
+        root: ROOT,
+        parser: 'bson',
+        callback: function() {
+
+          fs.readdirSync(ROOT).forEach(function(first) { // database
+
+            var second = ROOT + '/' + first;
+            if (!fs.statSync(second).isDirectory()) {
+              return;
+            }
+            fs.readdirSync(second).forEach(function(third) { // collections
+
+              var last = second + '/' + third;
+              if (!fs.statSync(last).isDirectory()) {
+                return;
+              }
+              fs.readdirSync(last).forEach(function(item) { // documents
+
+                assert.equal(extname(item), '.bson');
+                fs.unlinkSync(last + '/' + item);
+              });
+            });
+          });
+          done();
+        }
+      });
+    });
+  });
+});
