2371ccdccdf1fc534fbc53b08e54be4f8391ed1e
hex7c0 <carniellifrancesco@gmail.com>
2015-02-24 16:50:01 +0100

    examples: add web stream

---
+++ examples/web_stream.js
@@ -0,0 +1,35 @@
+'use strict';
+/**
+ * @file web stream example
+ * @module mongodb-backup
+ * @subpackage examples
+ * @version 0.0.0
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+var backup = require('../index.js'); // use require('mongodb-backup') instead
+
+/*
+ * use
+ */
+
+var http = require('http');
+http.createServer(function(req, res) {
+
+  res.writeHead(200, {
+    'Content-Type': 'application/x-tar'
+  });
+
+  backup({
+    uri: 'uri', // mongodb://<dbuser>:<dbpassword>@<dbdomain>.mongolab.com:<dbport>/<dbdatabase>
+    collections: [ 'logins' ],
+    tar: 'dump.tar',
+    stream: res
+  });
+
+}).listen(3000, '127.0.0.1');
